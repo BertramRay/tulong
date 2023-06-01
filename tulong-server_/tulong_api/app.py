cea6df0ecf79c91e746e8f2484f43eff16e3b9fe
ziyang <ziyang@dora.design>
2023-06-01 15:50:57 +0800

    feat(app): add hello world test

--- tulong-server_/tulong_api/app.py
+++ tulong-server_/tulong_api/app.py
@@ -7,7 +7,7 @@ from resources.parser import ImageParser
 from resources.uploader import ImageUploader
 from resources.processor import ImageExtractor, ImageStyleDetector
 
-app = Flask(__name__, static_folder='temp',)
+app = Flask(__name__, static_folder='temp', )
 Config = config["development"]
 app.config.from_object(Config)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
@@ -19,6 +19,12 @@ api.add_resource(ImageUploader, '/api/upload')
 api.add_resource(ImageExtractor, '/api/extract')
 api.add_resource(ImageStyleDetector, '/api/detect')
 api.add_resource(ImageParser, '/api/parse')
+
+
+@app.route('/')
+def hello():
+    return "<h1>" + "Hello World!" + "</h1>"
+
 
 if __name__ == "__main__":
     app.run()
